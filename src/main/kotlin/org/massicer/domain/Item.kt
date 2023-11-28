a3623faea4d65708f3bf745d47b2c8e65238ab23
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2023-11-28 11:01:42 +0100

    first draft

---
+++ src/main/kotlin/org/massicer/domain/Item.kt
@@ -0,0 +1,35 @@
+package org.massicer.domain
+
+
+typealias Name = String
+typealias TagLine = String
+typealias EthanolMilliliters = ULong // for 100 ml of solution
+typealias InternationaBitternesUnit = UByte
+typealias Food = String
+typealias Instruction = String
+sealed interface Item {
+    val name: Name
+    val tagline: TagLine
+
+
+    data class Beer(
+        override val name: Name,
+        override val tagline: TagLine,
+        val abv: EthanolMilliliters,
+        val ibu: InternationaBitternesUnit,
+        val foodPairing: Food
+    ) : Item
+
+    data class Cocktail(
+        override val name: Name,
+        override val tagline: TagLine,
+        val ingredients: Set<Ingredient>,
+        val instructions: List<Instruction>
+    ) : Item {
+        data class Ingredient(val name: String, val amount: Amount) {
+            data class Amount(val value: Double, val measureUnit: MeasureUnit) {
+                enum class MeasureUnit { CL }
+            }
+        }
+    }
+}
