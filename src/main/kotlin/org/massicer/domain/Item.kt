94f2125bb76a80d670163a065d216e7b0ed18aea
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2023-11-28 12:40:13 +0100

    progress

--- src/main/kotlin/org/massicer/domain/Item.kt
+++ src/main/kotlin/org/massicer/domain/Item.kt
@@ -6,22 +6,22 @@ typealias EthanolMilliliters = Float // for 100 ml of solution
 typealias InternationaBitternesUnit = Int
 typealias Food = String
 typealias Instruction = String
-abstract class Item (
+abstract class Item(
     val name: Name,
     val tagline: TagLine
-){
+) {
 
     init {
-        check(name.isNotEmpty() ) {"Name cannot be empty"}
-        check(name.isNotBlank() ) {"Name cannot be blank"}
-        check(tagline.isNotEmpty() ) {"Tagline cannot be empty"}
-        check(tagline.isNotBlank() ) {"Tagline cannot be blank"}
+        check(name.isNotEmpty()) { "Name cannot be empty" }
+        check(name.isNotBlank()) { "Name cannot be blank" }
+        check(tagline.isNotEmpty()) { "Tagline cannot be empty" }
+        check(tagline.isNotBlank()) { "Tagline cannot be blank" }
     }
 
-
+    @Suppress("MagicNumber")
     class Beer(
-         name: Name,
-         tagline: TagLine,
+        name: Name,
+        tagline: TagLine,
         val abv: EthanolMilliliters,
         val ibu: InternationaBitternesUnit,
         val foodPairing: Food
@@ -29,16 +29,17 @@ abstract class Item (
         init {
             check(foodPairing.isNotBlank()) { "FoodPairing cannot be blank" }
             check(foodPairing.isNotEmpty()) { "FoodPairing cannot be blank" }
+            check(abv >= 0.0f) { "ABV must be bigger or equal to 0" }
+            check(abv <= 100.0f) { "ABV must be smaller or equal to 100.0" }
         }
     }
 
-
-     class Cocktail(
-         name: Name,
-         tagline: TagLine,
+    class Cocktail(
+        name: Name,
+        tagline: TagLine,
         val ingredients: Set<Ingredient>,
         val instructions: List<Instruction>
-    ) :  Item(name, tagline) {
+    ) : Item(name, tagline) {
         data class Ingredient(val name: String, val amount: Amount) {
             data class Amount(val value: Double, val measureUnit: MeasureUnit) {
                 enum class MeasureUnit { CL }
--- src/test/kotlin/org/massicer/domain/ItemTest.kt
+++ src/test/kotlin/org/massicer/domain/ItemTest.kt
@@ -1,18 +1,16 @@
 package org.massicer.domain
 
-
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.throwables.shouldThrowWithMessage
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class ItemTest{
+class ItemTest {
 
     @Nested
-    inner class BeerTest{
+    inner class BeerTest {
 
         @Test
-        fun `name cannot be empty`(){
+        fun `name cannot be empty`() {
             shouldThrowWithMessage<IllegalStateException>("Name cannot be empty") {
                 Item.Beer(
                     name = "",
@@ -25,7 +23,7 @@ class ItemTest{
         }
 
         @Test
-        fun `name cannot be blank`(){
+        fun `name cannot be blank`() {
             shouldThrowWithMessage<IllegalStateException>("Name cannot be blank") {
                 Item.Beer(
                     name = " ",
@@ -50,21 +48,21 @@ class ItemTest{
             }
         }
 
-            @Test
-            fun `food pairing cannot be blank`() {
-                shouldThrowWithMessage<IllegalStateException>("FoodPairing cannot be blank") {
-                    Item.Beer(
-                        name = "mybeer",
-                        tagline = "Hello",
-                        abv = 0.12f,
-                        ibu = 2,
-                        foodPairing = " "
-                    )
-                }
+        @Test
+        fun `food pairing cannot be blank`() {
+            shouldThrowWithMessage<IllegalStateException>("FoodPairing cannot be blank") {
+                Item.Beer(
+                    name = "mybeer",
+                    tagline = "Hello",
+                    abv = 0.12f,
+                    ibu = 2,
+                    foodPairing = " "
+                )
             }
+        }
 
         @Test
-        fun `tagline cannot be empty`(){
+        fun `tagline cannot be empty`() {
             shouldThrowWithMessage<IllegalStateException>("Tagline cannot be empty") {
                 Item.Beer(
                     name = "my-name",
@@ -77,12 +75,38 @@ class ItemTest{
         }
 
         @Test
-        fun `tagline cannot be blank`(){
+        fun `tagline cannot be blank`() {
             shouldThrowWithMessage<IllegalStateException>("Tagline cannot be blank") {
                 Item.Beer(
                     name = "my-name",
                     tagline = " ",
                     abv = 0.12f,
+                    ibu = 2,
+                    foodPairing = "Pizza"
+                )
+            }
+        }
+
+        @Test
+        fun `abv cannot smallest than 0`() {
+            shouldThrowWithMessage<IllegalStateException>("ABV must be bigger or equal to 0") {
+                Item.Beer(
+                    name = "my-name",
+                    tagline = "ciao",
+                    abv = -0.1f,
+                    ibu = 2,
+                    foodPairing = "Pizza"
+                )
+            }
+        }
+
+        @Test
+        fun `abv cannot bigger than 100`() {
+            shouldThrowWithMessage<IllegalStateException>("ABV must be smaller or equal to 100.0") {
+                Item.Beer(
+                    name = "my-name",
+                    tagline = "ciao",
+                    abv = 100.1f,
                     ibu = 2,
                     foodPairing = "Pizza"
                 )
