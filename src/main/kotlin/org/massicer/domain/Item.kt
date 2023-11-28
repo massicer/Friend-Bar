7c2d3994bc1d3761940ecb406e215bcc81ab0517
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2023-11-28 11:20:56 +0100

    stop to use interface in base item

--- src/main/kotlin/org/massicer/domain/Item.kt
+++ src/main/kotlin/org/massicer/domain/Item.kt
@@ -1,16 +1,14 @@
 package org.massicer.domain
 
-
 typealias Name = String
 typealias TagLine = String
 typealias EthanolMilliliters = ULong // for 100 ml of solution
 typealias InternationaBitternesUnit = UByte
 typealias Food = String
 typealias Instruction = String
-sealed interface Item {
-    val name: Name
-    val tagline: TagLine
-
+sealed class Item {
+    abstract val name: Name
+    abstract val tagline: TagLine
 
     data class Beer(
         override val name: Name,
@@ -18,14 +16,14 @@ sealed interface Item {
         val abv: EthanolMilliliters,
         val ibu: InternationaBitternesUnit,
         val foodPairing: Food
-    ) : Item
+    ) : Item()
 
     data class Cocktail(
         override val name: Name,
         override val tagline: TagLine,
         val ingredients: Set<Ingredient>,
         val instructions: List<Instruction>
-    ) : Item {
+    ) : Item() {
         data class Ingredient(val name: String, val amount: Amount) {
             data class Amount(val value: Double, val measureUnit: MeasureUnit) {
                 enum class MeasureUnit { CL }
